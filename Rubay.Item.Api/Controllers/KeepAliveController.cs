723347a8b94ae9e2a2776879fa4b333e05fe51b6
LucaPicciau <74604421+LucaPicciau@users.noreply.github.com>
2021-04-30 11:54:41 +0200

    Add files via upload

---
+++ Rubay.Item.Api/Controllers/KeepAliveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rubay.Item.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class KeepAliveController : ControllerBase
+    {
+        [HttpGet]
+        public bool Get() => true;
+    }
+}
